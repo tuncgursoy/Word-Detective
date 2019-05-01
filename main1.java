019ab5f39bfe689dc626e21c1a8e24efe512adc4
Tunç Gürsoy <tuncgursoy@hotmail.com>
2019-05-02 01:11:31 +0300

    Prime15550 Update 2 02.05.2019

--- main1.java
+++
@@ -1,7 +0,0 @@
-public class main1
-{
-    public static void main(String[] args)
-    {
-        new StartScreen().setVisible(true);
-    }
-}
--- main2.java
+++
@@ -1,7 +0,0 @@
-public class main2
-{
-    public static void main(String[] args)
-    {
-        new StartScreen().setVisible(true);
-    }
-}
